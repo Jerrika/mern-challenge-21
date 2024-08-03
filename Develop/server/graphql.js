9e24a82b11c44708d6e55664d327cba441622985
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-08-03 16:27:29 -0500

    inital commit and addedd graphql file.

---
+++ Develop/server/graphql.js
@@ -0,0 +1,17 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
+const { typeDefs, resolvers } = require('./schemas');
+
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ token: req.headers.authorization })
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+);
